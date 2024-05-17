70e0f2e1a2b707ab2dfb29916b1cca8f26dff622
Vishvjeet-Rana <168952166+Vishvjeet-Rana@users.noreply.github.com>
2024-05-17 23:50:59 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all JS code as the newer version
+
+//alert("I am Vishvjeet Rana"); we are using nodejs not the browser
+
+let name = "Vishvjeet"
+let age = 18
+let canVote = true
+
+//number => in the range of 2 to power 53
+//use bigint => for bigger int values
+//boolean => true/false
+//symbol => unique type (will use it in the reactjs)
+//null => standalone value
+//undefined => which is not defined yet
+
+console.log(typeof "hello");
+console.log(typeof canVote);
+
+//type of null is an object in js
+//type of undefined is undefined
