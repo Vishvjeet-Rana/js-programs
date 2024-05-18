536b5a6035ec0c3115b81a77540638b6b23954d6
Vishvjeet-Rana <168952166+Vishvjeet-Rana@users.noreply.github.com>
2024-05-18 00:36:59 +0000

    conversion operations

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,37 @@
+let some = undefined
+let convertNum = Number(some) 
+// console.log(typeof convertNum); 
+// console.log(convertNum); 
+
+// NOTES - CONVERSION TO INT/NUMBER
+// "33" => 33
+// "33abcsjf" => NaN but typeof NaN is number
+// null => 0
+// undefined => NaN
+// true => 1 and false => 0
+
+
+let isLogged = 0
+let convertBool = Boolean(isLogged)
+// console.log(typeof convertBool);
+// console.log(convertBool);
+
+// NOTES - CONVERSION TO BOOLEAN
+// "" => false
+// "something" => true
+// null => false
+// undefined => false
+// any +ve or -ve value => true
+// 1 => true and 0 => false 
+
+
+let stringVal = undefined
+let convertString = String(stringVal) 
+console.log(typeof convertString)
+console.log(convertString);  
+
+// NOTES - CONVERTSION TO STRING
+// null => null - but as a string
+// undefined => undefined - but as a string
+// any number as it is but as a string
+// both true and false as it is but as a string
