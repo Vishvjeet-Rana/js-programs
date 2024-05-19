fc7a1c461adcb0acd0e1b015398080fb6f1792ba
Vishvjeet-Rana <168952166+Vishvjeet-Rana@users.noreply.github.com>
2024-05-19 00:45:47 +0000

    comparisions in js

---
+++ 01_basics/04_comparisions.js
@@ -0,0 +1,45 @@
+//we do comparision using == > < >= <= these operators
+//let do some fun
+
+// console.log("now we will play with zero");
+
+// console.log(0 == 0); //true
+// console.log(0 < 0); //false
+// console.log(0 > 0); //false
+// console.log(0 >= 0); //true
+// console.log(0 <= 0); //true
+
+// console.log("now we will play with null");
+
+// console.log(null == null); //true
+// console.log(null >= null); //true
+// console.log(null <= null); //true
+// console.log(null > null); //false
+// console.log(null < null); //false
+
+// console.log("now we will play with undefined");
+
+// console.log(undefined == undefined); //true
+// console.log(undefined <= undefined); //false
+// console.log(undefined >= undefined); //false
+// console.log(undefined < undefined); //false
+// console.log(undefined > undefined); //false
+
+//lets explore some more things with null and zero
+// console.log(null == 0);
+// console.log(null <= 0);
+// console.log(null >= 0);
+// console.log(null > 0);
+
+/*
+Imp to Note :- In Js, eequality checker == and comparision operators < > <= >=
+works differently.
+Comparision converts null to a number, treating it as 0. 
+That's why (3) null >= 0 gives true and (1) null > 0 false.
+*/
+
+console.log(undefined == 0); //false
+console.log(undefined < 0); //false
+console.log(undefined > 0); //false
+
+console.log(undefined == null);//true - because undefined loosely equal to only null
